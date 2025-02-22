90b7627f3e9e61590a07b5adeefb0f34c777211b
Rabi Deyashi <engg.r.deyashi@gmail.com>
2025-02-22 22:48:34 +0530

    probelm 8

--- src/app/problem-8/problem-8.component.ts
+++ src/app/problem-8/problem-8.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { ApiCallService } from './service/api-call.service';
+import { HttpClientModule } from '@angular/common/http';
+import { debounceTime, tap } from 'rxjs';
 
 @Component({
   selector: 'app-problem-8',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './problem-8.component.html',
-  styleUrl: './problem-8.component.scss'
+  styleUrl: './problem-8.component.scss',
+  providers: [ApiCallService]
 })
 export class Problem8Component {
+  constructor(private apiService: ApiCallService) { }
+  isLoading: boolean = false;
+  apiData: any[] = []
 
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.apiService.getUser().subscribe({
+      next: (result: any[]) => {
+        if (result) {
+          setTimeout(() => {
+            this.isLoading = false;
+            result.map((obj: any) => {
+              this.apiData.push({
+                name: obj.name,
+                email: obj.email,
+                phone: obj.phone
+              })
+            })
+          }, 300)
+        }
+        //this.isLoading = false;
+      },
+      error: error => {
+        console.error(error);
+      }
+    })
+  }
 }
