4979614ab4cf8ac216daf2226dc69380f71e16e3
Rabi Deyashi <engg.r.deyashi@gmail.com>
2025-02-24 19:46:45 +0530

    problem 12

--- src/app/problem-12/problem-12.component.ts
+++ src/app/problem-12/problem-12.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectCounter } from './store/counter.selector';
+import { decrement, increment } from './store/counter.actions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-problem-12',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './problem-12.component.html',
   styleUrl: './problem-12.component.scss'
 })
 export class Problem12Component {
+  count$!: Observable<number>;
 
+  constructor(private store: Store){
+    this.count$ = this.store.select(selectCounter);
+  }
+
+  increase(){
+    this.store.dispatch(increment());
+  }
+
+  decrease(){
+    this.store.dispatch(decrement());
+  }
 }
