f42c880e89ace3355bdecffded56b1f80feb4fe2
Rabi Deyashi <engg.r.deyashi@gmail.com>
2025-02-27 14:50:41 +0530

    problem-17

---
+++ src/app/problem-17/service/user.service.spec.ts
@@ -0,0 +1,43 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { UserService } from './user.service';
+
+describe('UserService', () => {
+  let service: UserService;
+  let httpMock: HttpTestingController
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
+    service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController)
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should fetch users from API', () => {
+    const dummyUsers = {
+      users: [
+        { id: 1, name: 'John Doe' },
+        { id: 2, name: 'Jane Doe' }
+      ]
+    }
+    service.getUsers().subscribe(users => {
+      expect(users).toEqual(dummyUsers);
+      expect(users.users.length).toBe(2);
+      expect(users.users[0].name).toBe('John Doe');
+    })
+
+    const req = httpMock.expectOne('https://dummyjson.com/users');
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyUsers);
+  })
+});
